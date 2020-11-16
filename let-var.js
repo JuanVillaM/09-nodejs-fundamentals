1124cc68afb96390e8863f4d61bba164ea1362b1
Juan Villa <juanestebanvillamemdoza@gmail.com>
2020-11-15 20:29:10 -0400

    Adding let-var.js file

---
+++ let-var.js
@@ -0,0 +1,13 @@
+let name = 'Wolverine';
+if (true) {
+    name = 'Magneto';
+}
+
+console.log(name);
+
+let i = 'Hello World';
+
+for (let i = 0; i <= 5; i++) {
+    console.log(`i: ${i}`);
+}
+console.log(i);
